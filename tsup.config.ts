85014d3fa35da5534ce1fcb767c0b73e0efed2dd
nurodev <me@nuro.dev>
2021-10-05 00:52:24 +0100

    Initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import type { Options } from "tsup";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export const tsup: Options = {
+  clean: true,
+  dts: true,
+  entryPoints: ["src/index.ts"],
+  sourcemap: true,
+  minify: isProduction,
+};
