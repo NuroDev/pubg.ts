35bc2e0842b62abd02eb3e23860eb8f0695fbd32
nurodev <me@nuro.dev>
2021-10-05 11:13:33 +0100

    Added basic WindiCSS setup

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,3 +1,19 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <div class="container">
+    <div class="content">
+      <h1>Hello World</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+  .container {
+    @apply max-w-7xl mx-auto px-4 sm:px-6 lg:px-8;
+  }
+
+  .content {
+    @apply max-w-3xl mx-auto;
+  }
+</style>
--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
+import WindiCSS from "vite-plugin-windicss";
 
 export default defineConfig({
   root: "example",
-  plugins: [Vue()],
+  plugins: [
+    Vue(),
+    WindiCSS({
+      config: {
+        darkMode: "class",
+      },
+    }),
+  ],
 });
