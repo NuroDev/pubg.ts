84d205d60060218b3070789dac25944a8f6ebec9
nurodev <me@nuro.dev>
2021-10-05 21:40:25 +0100

    Removed api endpoint example

--- example/api/player.ts
+++
@@ -1,5 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  res.send("Hello Player");
-}
