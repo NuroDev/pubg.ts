4ba4fee519036c455f589e703bb6521a319b431d
nurodev <me@nuro.dev>
2021-10-08 00:00:38 +0100

    Added samples endpoint hook

--- src/hooks/useSamples.ts
+++ src/hooks/useSamples.ts
@@ -1,25 +1,73 @@
-import { ErrorCode } from "..";
+import { ErrorCode, Shard } from "..";
 import { fetch } from "../util";
 
-import type { BaseResponse } from "..";
+import type {} from "..";
 import type { WithApiShard } from "../types/util";
 
-export interface SamplesOptions extends WithApiShard {}
+export interface SamplesOptions extends WithApiShard {
+  /**
+   * The starting search date for the matches in UTC
+   *
+   * @default now() - 24hrs
+   */
+  createdAt?: Date;
+}
 
-export interface SamplesResponse extends BaseResponse {}
+export interface SamplesResponse {
+  data: {
+    /**
+     * Time of samples list creation
+     */
+    createdAt: Date;
+
+    /**
+     * Platform shard
+     */
+    shardId: Shard;
+
+    /**
+     * Identifies the studio & game
+     */
+    titleId: string;
+  };
+  relationships: {
+    matches: {
+      /**
+       * An array of sample matches containing their IDs & shards
+       */
+      data: Array<{
+        /**
+         * Match ID
+         *
+         * Used to lookup the full match object on the /matches endpoint
+         */
+        id: string;
+        /**
+         * Identifier for this object type
+         */
+        type: string;
+      }>;
+    };
+  };
+}
 
 /**
  * Get a list of all past matches from the api
  *
- * @todo
+ * @todo Test this functions correctly
  *
  * @param {Object} options - Sample Options
  */
-export async function useSamples({ ...rest }: SamplesOptions) {
+export async function useSamples({ createdAt, ...rest }: SamplesOptions) {
   try {
     return await fetch<SamplesResponse>({
       ...rest,
-      endpoint: "",
+      endpoint: "samples",
+      params: createdAt
+        ? {
+            "filter[createdAt]": createdAt.toISOString(),
+          }
+        : undefined,
     });
   } catch (error) {
     console.error(ErrorCode.HOOK_FETCH_SAMPLES, error);
