d0b54edcd56be88fa9b607041187dba91550d503
nurodev <me@nuro.dev>
2021-10-07 23:09:57 +0100

    Updated WithApiShard type

--- src/types/util.ts
+++ src/types/util.ts
@@ -1,15 +1,20 @@
 import type { Links, Shard } from "./index";
 
-export type WithApiShard<T = {}> = T & {
+export type WithApiKey<T = {}> = T & {
   /**
    * PUBG Developer App API access token
    */
   apiKey: string;
+};
+
+export type WithShard<T = {}> = T & {
   /**
    * The server shard to send the request to
    */
   shard?: Shard;
 };
+
+export type WithApiShard<T = {}> = T & WithApiKey & WithShard;
 
 export type WithLinks<T = {}> = T & {
   /**
