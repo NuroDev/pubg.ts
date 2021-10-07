32f031d5fa9aae3166bb24ca622d0c09d36876fb
nurodev <me@nuro.dev>
2021-10-08 00:17:21 +0100

    Added initial telemetry endpoint hook

--- src/hooks/useTelemetry.ts
+++ src/hooks/useTelemetry.ts
@@ -1,26 +1,40 @@
+import axios from "axios";
+
+import { BASE_HEADERS } from "../constants";
 import { ErrorCode } from "..";
-import { fetch } from "../util";
+
+import type { AxiosResponse } from "axios";
 
 import type { BaseResponse } from "..";
-import type { WithApiShard } from "../types/util";
+import type { WithApiKey } from "../types/util";
 
-export interface TelemetryOptions extends WithApiShard {}
+export interface TelemetryOptions extends WithApiKey {
+  /**
+   * URL of the telemetry object
+   */
+  url: string;
+}
 
 export interface TelemetryResponse extends BaseResponse {}
 
 /**
- * Fetches telemetry data
+ * Fetches telemetry data from a provided URL
  *
- * @todo
+ * @todo Test this functions correctly
  *
  * @param {Object} options - Telemetry Options
  */
-export async function useTelemetry({ ...rest }: TelemetryOptions) {
+export async function useTelemetry({ apiKey, url }: TelemetryOptions) {
   try {
-    return await fetch<TelemetryResponse>({
-      ...rest,
-      endpoint: "",
+    const { data }: AxiosResponse<TelemetryResponse> = await axios(url, {
+      headers: {
+        ...BASE_HEADERS,
+        Authorization: `Bearer ${apiKey}`,
+      },
+      responseType: "json",
     });
+
+    return data;
   } catch (error) {
     console.error(ErrorCode.HOOK_FETCH_TELEMETRY, error);
     throw error;
