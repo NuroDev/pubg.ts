f10ebf748e7cb65e10ba3bcfc7239e83e8ebd019
nurodev <me@nuro.dev>
2021-11-03 00:55:55 +0000

    Added tests common utils / data

---
+++ tests/common.ts
@@ -0,0 +1,9 @@
+export const playerNames = ["iBBA", "WackyJacky101"];
+
+export const playerIds = [
+  "account.1225daa4e7544edfbe5f115b433d5069", // iBBA
+  "account.c0e530e9b7244b358def282782f893af", // WackyJacky101
+];
+
+export const matchUUID =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
--- tests/data.ts
+++
@@ -1,8 +0,0 @@
-export const playerNames = ["iBBA", "WackyJacky101"];
-
-export const playerIds = [
-  "account.1225daa4e7544edfbe5f115b433d5069",
-  "account.c0e530e9b7244b358def282782f893af",
-];
-
-export const matchIds = [];
